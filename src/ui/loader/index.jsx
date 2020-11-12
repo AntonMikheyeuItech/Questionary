859a719b05cb6984af904ecba84a414378b59d3b
Anton Mikheyeu <anton.mikheyeu@itechart-group.com>
2020-11-12 15:11:57 +0300

    Main template was created

---
+++ src/ui/loader/index.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const Loader = () => {
+    const [dots, setDots] = useState([]);
+
+    setTimeout(() => (
+        setDots(dots => (
+            dots.length === 3
+                ? []
+                : [...dots, "."]
+        ))
+    ), 1000);
+
+    return (
+        <div className="loader" >
+            <img className="loader__logo" src="https://itechart-by.s3.amazonaws.com/storage/static/svg/logo.svg" alt="Loading"/>
+            <h1 className="loader__dots">{dots.join("")}</h1>
+        </div>
+    );
+};
+
+export default Loader;
